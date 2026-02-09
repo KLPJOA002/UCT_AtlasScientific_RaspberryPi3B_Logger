c51d0d83b2782d6c429749f39049a8a41c2c571c
Joab Gray Kloppers <klpjoa002@myuct.ac.za>
2026-02-09 20:41:06 +0200

    Included existing script to scan for usbDrives. Using dummy function to store data in drive.

--- Code/AutomatedLogger.py
+++ Code/AutomatedLogger.py
@@ -5,10 +5,109 @@ import time
 import copy
 import string
 #from AtlasI2C import (AtlasI2C)
+from Media_Scanner import *
+from util import *
+from database import *
+from log import *
 
+# Source - https://stackoverflow.com/a/6935354
+# Posted by Michael Dillon
+# Retrieved 2026-02-09, License - CC BY-SA 3.0
+
+def write_and_verify(f_n,data):
+    f = file(f_n,'w')
+    f.write(data)
+    f.flush()
+    os.fsync(f.fileno())
+    f.close()
+    f = file(f_n,'r')
+    verified = f.read()
+    f.close()
+    return  verified == data and f.closed
+
+# Source - https://stackoverflow.com/q/6840711
+# Posted by Ken, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-02-09, License - CC BY-SA 3.0
+
+def get_partition(dev):
+    os.system('fdisk -l %s > output' % dev)
+    f = file('output')
+    data = f.read()
+    print data
+    f.close()
+    return data.split('\n')[-2].split()[0].strip()
+
+def mount_partition(partition):
+    os.system('mount %s /media/mymntpnt' % partition)
+
+
+def unmount_partition():
+    os.system('umount /media/mymntpnt')
+
+
+#function obtains an array of all Atlas scientific devices connected to the I2C bus
+def get_devices():
+    device = AtlasI2C()
+    device_address_list = device.list_i2c_devices()
+    device_list = []
+    
+    for i in device_address_list:
+        device.set_i2c_address(i)
+        response = device.query("I")
+        try:
+            moduletype = response.split(",")[1] 
+            response = device.query("name,?").split(",")[1]
+        except IndexError:
+            print(">> WARNING: device at I2C address " + str(i) + " has not been identified as an EZO device, and will not be queried") 
+            continue
+        device_list.append(AtlasI2C(address = i, moduletype = moduletype, name = response))
+    return device_list 
+
+def read():
+    #get array of connected devices
+    Out = []
+    device_list = get_devices()
+    
+    #get the first device in the list to be used to get the time delays required.
+    DeviceTemplate = device_list[0]
+    delaytime = DeviceTemplate.LONG_TIMEOUT
+
+
+    for dev in device_list:
+        dev.write("R")
+    time.sleep(delaytime)
+    for dev in device_list:
+        Out.append(dev.read())
+        
+    return Out
+    
+#dummy function to test just values
+def dummy():
+    
+    for i in range(2):
+        time.sleep(0.1)
+    return ["Success DO ---------- 10.2","Success PTD ---------- 20000"]
 
 def main():
-    print("test")
+    ms = MediaScanner()
+    
+    dataRaw = []
+    for i in range(10):
+        print(i)
+        dataRaw.append([time.ctime(), dummy()[0].split(" ")[1], dummy()[0].split(" ")[3]])
+        dataRaw.append([time.ctime(), dummy()[1].split(" ")[1], dummy()[1].split(" ")[3]])
+    
+    
+    Drives = ms.scan_media()
+    
+    if Drives:
+        for Drive in Drives:
+            mount_partition(get_partition(Drives))
+            write_and_verify('/media/mymntpnt/test_Write',dataRaw)
+            unmount_partition()
+    
+    print(dataRaw)
+    
     
     
 if __name__=='__main__':
---
+++ Code/Media_Scanner.py
@@ -0,0 +1,59 @@
+# Source - https://stackoverflow.com/q/6840711
+# Posted by Ken, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-02-09, License - CC BY-SA 3.0
+
+class MediaScanner():
+    def __init__(self):
+        self.lok = thread.allocate_lock()
+        self.running = True
+        self.started = False
+
+    def register_cb(self,func):
+        self.cb = func
+
+    def start(self):
+        if not self.started:
+            thread.start_new_thread(self.scan_thread,())
+
+    def scan_thread(self):
+        self.quit = False
+        self.started = True
+        last_devices = []
+        while self.running:
+            devices = self.scan_media()
+            if (devices != last_devices):
+                self.cb(devices) #call the callback as its own thread
+            last_devices = devices
+
+            time.sleep(0.1)
+
+        self.quit = True    
+
+    def stop(self):
+        self.running = False
+        while(not self.quit):
+            pass
+        return True
+
+    def is_running(self):
+        return self.running
+
+
+    def scan_media(self):
+        with self.lok:
+            partitionsFile = open("/proc/partitions")
+            lines = partitionsFile.readlines()[2:]#Skips the header lines
+            devices = []
+            for line in lines:
+                words = [x.strip() for x in line.split()]
+                minorNumber = int(words[1])
+                deviceName = words[3]
+                if minorNumber % 16 == 0:
+                    path = "/sys/class/block/" + deviceName
+                    if os.path.islink(path):
+                        if os.path.realpath(path).find("/usb") > 0:
+                            devices.append('/dev/'+deviceName)
+
+            partitionsFile.close()
+
+            return devices
