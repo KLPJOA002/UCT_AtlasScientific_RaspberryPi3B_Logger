90599de06dd07cdb05dc82ae421c606556687698
Joab Gray Kloppers <klpjoa002@myuct.ac.za>
2026-02-09 19:14:34 +0200

    Created Repository for the automated logger.

---
+++ Code/AutomatedLogger.py
@@ -0,0 +1,16 @@
+import io
+import sys
+#import fcntl
+import time
+import copy
+import string
+#from AtlasI2C import (AtlasI2C)
+
+
+def main():
+    print("test")
+    
+    
+if __name__=='__main__':
+    main()
+    
