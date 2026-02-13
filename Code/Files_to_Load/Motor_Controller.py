699cacc229f30c690a1dc2561d0a5ae0dbfd548c
Joab Gray Kloppers <klpjoa002@myuct.ac.za>
2026-02-13 18:16:18 +0200

    Stage with the current setup

--- Code/Files_to_Load/Motor_Controller.py
+++ Code/Files_to_Load/Motor_Controller.py
@@ -5,13 +5,14 @@
 import RPi.GPIO as GPIO
 import time
 
-Frequency = 0.1 #define the frequency and duty cycle of the motorised pump
-Duty_Cycle = 0.5
+Period = 10 #define the Period and duty cycle of the motorised pump (A single On-Off cycle is one period: on for 2 seconds, off for 4 = a 6s period)
+Duty_Cycle = 1 #duty cycle is the percentage of time the motor is on for the period. e.g. 10s period with 0.2 duty cycle = on for 2 seconds, off for 8
 
 Default_Mode = 0 #Default mode to determing weather the motor is on by default or off by default. 0: Motor on when GPIO pin goes high, 1: Motor off when GPIO pin goes high
 
 
 def main():
+    Frequency = 1/Period
     if not Default_Mode: #assign the correct pin duty cycle to match the desired motor duty cycle depending on default motor mode.
         Pin_High_Time = (1/Frequency)*Duty_Cycle
         Pin_Low_Time = (1/Frequency)-Pin_High_Time
